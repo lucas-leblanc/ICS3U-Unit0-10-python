29468ff5da668c406df02f110995cb092884cf59
Lucas LeBlanc <lucas.leblanc@mths.ca>
2022-10-15 03:51:38 +0000

    added file

---
+++ adding.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Lucas LeBlanc
+# Created on: Oct 2022
+# This program adds 0.1 and 0.2
+
+
+def main():
+    # This function adds 0.1 and 0.2
+
+    the_sum = 0.1 + 0.2
+    print("{0} is the sum of 0.1 + 0.2".format(the_sum))
+
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
